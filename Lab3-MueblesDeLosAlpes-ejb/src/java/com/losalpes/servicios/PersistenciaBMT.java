d2e6ab1f39f47b80feb86770110f4642db2248aa
za.garcia10 <za.garcia10@ISC325.sis.virtual.uniandes.edu.co>
2015-10-21 20:41:21 -0500

    Implementación BMT

--- Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/PersistenciaBMT.java
+++ Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/PersistenciaBMT.java
@@ -6,28 +6,29 @@
 package com.losalpes.servicios;
 
 import com.losalpes.entities.Vendedor;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.annotation.Resource;
+import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
 import javax.sql.DataSource;
-import javax.transaction.HeuristicMixedException;
-import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
-import javax.transaction.RollbackException;
-import javax.transaction.SystemException;
 import javax.transaction.UserTransaction;
 
 /**
  *
  * @author za.garcia10
  */
+@Stateless
 @TransactionManagement(TransactionManagementType.BEAN)
 public class PersistenciaBMT {
     
     @Resource 
     private UserTransaction ut;
     
-    @Resource(mappedName = "jdbc/derbyDatasource") 
+    @Resource(mappedName = "jdbc/derbyDS") 
     private DataSource dataSource;
     
     public void initTransaction() throws Exception {
@@ -43,11 +44,43 @@ public class PersistenciaBMT {
     }
     
     public void insertRemoteDatabase(Vendedor vendedor){
-        
+        try {
+            initTransaction();
+            StringBuilder sql = new StringBuilder();
+            sql.append("insert into vendedores (identificacion, nombres, apellidos) ");
+            sql.append("values (?,?,?)");
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql.toString());
+            statement.setString(1, String.valueOf(vendedor.getIdentificacion()));
+            statement.setString(2, vendedor.getNombres());
+            statement.setString(3, vendedor.getApellidos());
+            statement.executeQuery();
+            commitTransaction();
+        } catch (Exception ex) {
+            try {
+                rollBackTransaction();
+            } catch (Exception ex1) {
+                Logger.getLogger(PersistenciaBMT.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        } 
     }
     
     public void deleteRemoteDatabase(Vendedor vendedor){
-        
+        try {
+            initTransaction();
+            final String sql = "delete from vendedores v where v.identificacion = ?";
+            Connection connection = dataSource.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, String.valueOf(vendedor.getIdentificacion()));
+            statement.executeQuery();
+            commitTransaction();
+        } catch (Exception ex) {
+            try {
+                rollBackTransaction();
+            } catch (Exception ex1) {
+                Logger.getLogger(PersistenciaBMT.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        }
     }
     
     
