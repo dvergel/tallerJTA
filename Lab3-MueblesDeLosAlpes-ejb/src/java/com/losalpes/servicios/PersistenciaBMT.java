8d5d30e8814e839b5c35d2a9972f3bb0c264ffa1
Zamir García <zgarcia83@gmail.com>
2015-10-24 23:40:49 -0500

    Compra BMT

--- Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/PersistenciaBMT.java
+++ Lab3-MueblesDeLosAlpes-ejb/src/java/com/losalpes/servicios/PersistenciaBMT.java
@@ -5,9 +5,14 @@
  */
 package com.losalpes.servicios;
 
+import com.losalpes.entities.Mueble;
+import com.losalpes.entities.TarjetaCreditoAlpes;
+import com.losalpes.entities.Usuario;
 import com.losalpes.entities.Vendedor;
+import com.losalpes.excepciones.CupoInsuficienteException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
@@ -83,6 +88,35 @@ public class PersistenciaBMT {
         }
     }
     
+    public void comprar(List<Mueble> muebles, Usuario usuario, 
+            TarjetaCreditoAlpes tarjetaCreditoAlpes) throws CupoInsuficienteException, Exception{
+        double cupo = tarjetaCreditoAlpes.getCupo();
+        double total = totalizar(muebles);
+        
+        if (cupo - total < 0) {
+            throw  new CupoInsuficienteException("Cupo insuficiente para la compra");
+        }
+        tarjetaCreditoAlpes.setCupo(cupo - total) ;
+        StringBuilder sql = new StringBuilder();
+        sql.append("update TARJETA_CREDITO_ALPES tc set tc.CUPO=? ");
+        sql.append("where tc.IDENTIFICACION=?");
+        
+        initTransaction();
+        Connection connection = dataSource.getConnection();
+        PreparedStatement statement = connection.prepareStatement(sql.toString());
+        statement.setDouble(1, cupo - total);
+        statement.setString(2, tarjetaCreditoAlpes.getIdentificacion());
+        statement.executeQuery();
+        commitTransaction();
+    }
+
+    private double totalizar(List<Mueble> muebles) {
+        double total = 0.0;
+        for (Mueble mueble: muebles) {
+            total += mueble.getPrecio();
+        }
+        return total;
+    }
     
     
 }
