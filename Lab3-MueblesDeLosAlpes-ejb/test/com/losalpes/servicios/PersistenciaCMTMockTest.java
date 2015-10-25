e392e4f955ac718c58317479428537902a46df22
wilman rincon <wrincon_b@hotmail.com>
2015-10-24 23:49:06 -0500

    clase test

--- Lab3-MueblesDeLosAlpes-ejb/test/com/losalpes/servicios/PersistenciaCMTMockTest.java
+++ Lab3-MueblesDeLosAlpes-ejb/test/com/losalpes/servicios/PersistenciaCMTMockTest.java
@@ -6,7 +6,9 @@
 package com.losalpes.servicios;
 
 import com.losalpes.entities.Vendedor;
+import com.losalpes.entities.TarjetaCreditoAlpes;
 import com.losalpes.excepciones.OperacionInvalidaException;
+import java.sql.Date;
 import java.util.Properties;
 import javax.naming.InitialContext;
 import static org.junit.Assert.assertFalse;
@@ -26,6 +28,12 @@ public class PersistenciaCMTMockTest {
      * Interface con referencia al servicio de vendedores en el sistema
      */
     private IServicioVendedoresMockRemote servicio;
+   
+    /**
+     * Interface con referencia al servicio de tarjeta en el sistema
+     */
+    private IServicioTarjetaCreditoMockRemote servicioTarjeta;
+   
     
     /**
      * Método que se ejecuta antes de comenzar la prueba unitaria
@@ -43,6 +51,7 @@ public class PersistenciaCMTMockTest {
             InitialContext contexto;
             contexto = new InitialContext(env);
             servicio = (IServicioVendedoresMockRemote) contexto.lookup("com.losalpes.servicios.IServicioVendedoresMockRemote");
+            servicioTarjeta = (IServicioTarjetaCreditoMockRemote) contexto.lookup("com.losalpes.servicios.IServicioTarjetaCreditoMockRemote");
         } 
         catch (Exception e)
         {
@@ -78,6 +87,47 @@ public class PersistenciaCMTMockTest {
         
         try {
             servicio.eliminarVendedor(1L);
+        } catch (OperacionInvalidaException e) {
+            thrown = true;
+        }
+        
+        assertFalse(thrown);
+    }
+    
+    /**
+     * Método de prueba para agregar una tarjeta al sistema 
+     */
+    @Test(expected=OperacionInvalidaException.class)
+    public void testAgregarTarjeta () throws Exception
+    {
+         boolean thrown = false;
+        TarjetaCreditoAlpes tarjeta = null;
+        tarjeta.setCupo(2345.9);
+        tarjeta.setIdentificacion("6543");
+        tarjeta.setNombreBanco("Banco Alpes");
+        tarjeta.setNombreTitutlar("pepe");
+        tarjeta.setFechaExpedicion(Date.valueOf("25/10/2015"));
+        tarjeta.setFechaVencimiento(Date.valueOf("25/10/2016"));
+        
+        try {
+            servicioTarjeta.agregarTarjeta(tarjeta);
+        } catch (OperacionInvalidaException e) {
+            thrown = true;
+        }
+        
+        assertFalse(thrown);
+    }
+    
+    /**
+     * Método de prueba para eliminar una tarjeta al sistema
+     */
+    @Test(expected=OperacionInvalidaException.class)
+    public void testEliminarTarjeta() throws Exception
+    {
+        boolean thrown = false;
+        
+        try {
+            servicioTarjeta.eliminarTarjeta("2");
         } catch (OperacionInvalidaException e) {
             thrown = true;
         }
